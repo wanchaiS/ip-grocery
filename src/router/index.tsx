003d0098565b875efa2560426c81ae97b763b447
wanchais <wanchai.sangkusolwong@gmail.com>
2025-04-15 00:40:42 +1000

    fix router browser basename

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,29 +4,32 @@ import OrderPage from "@/pages/order/OrderPage";
 import { createBrowserRouter, Navigate } from "react-router";
 import Providers from "../Providers";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Providers />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/home" replace />,
-      },
-      {
-        path: "/home",
-        element: <HomePage />,
-      },
-      {
-        path: "/cart",
-        element: <CartPage />,
-      },
-      {
-        path: "/order",
-        element: <OrderPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Providers />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to="/home" replace />,
+        },
+        {
+          path: "/home",
+          element: <HomePage />,
+        },
+        {
+          path: "/cart",
+          element: <CartPage />,
+        },
+        {
+          path: "/order",
+          element: <OrderPage />,
+        },
+      ],
+    },
+  ],
+  { basename: "/ip-grocery" }
+);
 
 export default router;
